fcaf6b8db03d376e0d019c9f120cfd96b7c5f1bd
iccol <106884783+iccol@users.noreply.github.com>
2022-09-30 11:36:29 -0700

    Add files via upload

---
+++ client/src/MyRouter.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import NoMatch from './General/components/NoMatch.js';
+import Booking from './Booking/containers/Booking.js';
+import InitDb from './General/InitDb.js';
+
+class MyRouter extends Component {
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Booking} />
+          <Route exact path="/init" component={InitDb} />
+          <Route exact path="/booking" component={Booking} />
+          <Route path="*" component={NoMatch} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default MyRouter;
